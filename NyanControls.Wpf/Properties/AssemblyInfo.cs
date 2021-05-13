d41328c3a7daa6626f1a8ef13347e64fd9effa1d
Ian Good <Ian.M.Good@gmail.com>
2021-05-12 22:48:47 -0700

    updated Wpf projects to net5

--- NyanControls.Wpf/Properties/AssemblyInfo.cs
+++ NyanControls.Wpf/Properties/AssemblyInfo.cs
@@ -1,39 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows.Markup;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NyanControls.Wpf")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NyanControls.Wpf")]
-[assembly: AssemblyCopyright("Copyright © 2018 Ian Good")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ff4924d5-b91e-4cc0-80ff-1ffcebb28f17")]
+﻿using System.Windows.Markup;
 
 [assembly: XmlnsPrefix("https://github.com/IGood/NyanControls", "nyan")]
 [assembly: XmlnsDefinition("https://github.com/IGood/NyanControls", "NyanControls.Wpf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
